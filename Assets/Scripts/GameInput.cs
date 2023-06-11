f43d1786736212f640fb7519ee0dc7298fa8522c
Ada Wykowska <adejastrzebiecwykowska@edu.cdv.pl>
2023-06-11 15:28:35 +0200

    Scripts update for new interaction

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,15 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
+
+
     private InputActions playerInputActions;
     private void Awake()
     {
         playerInputActions = new InputActions();
         playerInputActions.Player.Enable();
+        playerInputActions.Player.Interaction.performed += Interaction_performed;
     }
+
+    private void Interaction_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     private bool isWalking;
     [SerializeField] private float speed = 5f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private float interactDistance = 2f;
+    [SerializeField] private LayerMask countersLayerMask;
+
+    private Vector3 lastInteractDir;
     void Update()
+    {
+        HandleMovement();
+        //HandleInteractions();
+    }
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        if(moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        if(Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+            Debug.Log(raycastHit.transform);
+        } 
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -21,19 +70,17 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
-            if(canMove)
+            if (canMove)
             {
                 moveDir = moveDirX;
-            } else
+            }
+            else
             {
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
-                if(canMove)
+                if (canMove)
                 {
                     moveDir = moveDirZ;
-                } else
-                {
-
                 }
             }
         }
@@ -44,7 +91,6 @@ public class PlayerController : MonoBehaviour
         isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        Debug.Log(inputVector);
     }
 
     public bool IsWalking()
