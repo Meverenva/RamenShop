6bccb9d0ddf014fe3a13cee137dc6305f76f3053
Ada Wykowska <adejastrzebiecwykowska@edu.cdv.pl>
2023-06-12 02:04:12 +0200

    Container Counter script

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerCounter : BaseCounter, IKitchenObjectParent
+{
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private KitchenObject_SO kitchenObjectSO;
+
+    private KitchenObject kitchenObject;
+
+    public override void Interact(PlayerController player)
+    {
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+        }
+        else
+        {
+            kitchenObject.SetKitchenObjectParent(player);
+        }
+    }
+
+
+    public Transform GetKichenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
