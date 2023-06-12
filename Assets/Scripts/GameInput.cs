6d3fe2fc3c431b6ccda8085c0ad8941f5f876a50
Ada Wykowska <adejastrzebiecwykowska@edu.cdv.pl>
2023-06-12 21:54:53 +0200

    Updated GameInput for second interaction

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class GameInput : MonoBehaviour
 {
     public event EventHandler OnInteractAction;
+    public event EventHandler OnInteractAlternateAction;
 
 
     private InputActions playerInputActions;
@@ -14,6 +15,12 @@ public class GameInput : MonoBehaviour
         playerInputActions = new InputActions();
         playerInputActions.Player.Enable();
         playerInputActions.Player.Interaction.performed += Interaction_performed;
+        playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
+    }
+
+    private void InteractAlternate_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAlternateAction?.Invoke(this, EventArgs.Empty);
     }
 
     private void Interaction_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
